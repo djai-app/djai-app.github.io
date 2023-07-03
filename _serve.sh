c66708102ecd41b5106f257619ac3d115131e0ef
Vova Bilous <vova.bilous@hotmail.com>
2023-07-03 23:47:35 +0300

    Update _serve.sh

--- _serve.sh
+++ _serve.sh
@@ -4,7 +4,7 @@ DOCKER_CMD="${DOCKER_CMD:-docker}"
 
 echo "Using docker command: $DOCKER_CMD"
 
-echo "Building container image to use for Apicurio Website development."
+echo "Building container image to use for DJAI Website development."
 docker build -t apicurio/website-dev .
-echo "Starting the Apicurio Website development container image."
+echo "Starting the DJAI Website development container image."
 docker run -it -p 8080:8080 -v $(pwd):/site:Z apicurio/website-dev
